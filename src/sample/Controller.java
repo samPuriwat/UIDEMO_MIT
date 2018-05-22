eb8a7eac84ba1fed2b9acbca0b2dada02aa23240
samPuriwat <sam.puriwat@gmail.com>
2018-05-22 16:40:10 +0700

    Connect SQLite: User Authentication

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,11 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import javax.swing.*;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -21,16 +23,41 @@ public class Controller implements Initializable {
     private PasswordField password;
     @FXML
     private Button btnLogin;
+    @FXML
+    private Label dbStatus;
+
+    private loginModel loginModel = new loginModel();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        if (this.loginModel.isDatabaseConnection()) {
+            this.dbStatus.setText("Connected to DB.");
+        } else {
+            this.dbStatus.setText("Not Connect to DB.");
+        }
+
+
 
     }
 
     @FXML
     public void Login(ActionEvent event){
+//        try {
+//            if (id.equals(username.getText()) && pass.equals(password.getText())) {
+//                JOptionPane.showMessageDialog(null,
+//                        "Welcome to MIT SAIYAI APP");
+//            } else {
+//                JOptionPane.showMessageDialog(null,
+//                        "Your username or password is invalid.",
+//                        "Warning Message",
+//                        JOptionPane.WARNING_MESSAGE);
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         try {
-            if (id.equals(username.getText()) && pass.equals(password.getText())) {
+            if (this.loginModel.isLogin(this.username.getText(), this.password.getText())) {
                 JOptionPane.showMessageDialog(null,
                         "Welcome to MIT SAIYAI APP");
             } else {
@@ -39,8 +66,7 @@ public class Controller implements Initializable {
                         "Warning Message",
                         JOptionPane.WARNING_MESSAGE);
             }
-
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -3,6 +3,8 @@ package sample;
 import dbUtil.dbConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class loginModel {
@@ -21,6 +23,37 @@ public class loginModel {
     public boolean isDatabaseConnection(){
         return this.connection != null;
     }
+
+    public boolean isLogin(String username, String password) throws SQLException {
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+
+        String sql = "select * from admin where username = ? and password = ?";
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1,username);
+            pr.setString(2,password);
+
+            rs = pr.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        finally {
+           pr.close();
+           rs.close();
+        }
+
+    }
+
+
+
+
+
 
 
 }//class
